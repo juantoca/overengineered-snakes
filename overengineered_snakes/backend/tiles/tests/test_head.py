c3b560e7510a065774eae3c6600aef25e42a364f
Juan Toca <elan17.programacion@gmail.com>
2023-01-04 18:39:01 +0100

    Added test for Head.move method

--- overengineered_snakes/backend/tiles/tests/test_head.py
+++ overengineered_snakes/backend/tiles/tests/test_head.py
@@ -1,4 +1,9 @@
+from typing import Tuple
+
+from hypothesis import assume
 from hypothesis import given
+from hypothesis.strategies import integers
+from hypothesis.strategies import tuples
 
 from overengineered_snakes.backend.mapa import Mapa
 from overengineered_snakes.backend.tests.strategies.mapa import empty_mapa_st
@@ -12,3 +17,14 @@ def test_head_die(head: Head, mapa: Mapa) -> None:
     mapa.set_coords(head.coords, head)
     head.die(mapa)
     assert type(mapa.get_coords(head.coords)) is Body
+
+
+@given(head_st, empty_mapa_st, tuples(integers(), integers()))
+def test_head_move(head: Head, mapa: Mapa, coords: Tuple[int, int]) -> None:
+    original_coords = head.coords
+    mapa.set_coords(head.coords, head)
+    assume(mapa.get_coords(coords) is not head)
+    head.move(coords, mapa)
+    assert type(mapa.get_coords(original_coords)) is Body
+    assert type(mapa.get_coords(coords)) is Head
+    assert head.nextone == original_coords

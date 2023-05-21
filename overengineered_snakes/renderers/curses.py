b3f73bccceba728ee8c7e3e42c31662d2f4f5675
Juan Toca <elan17.programacion@gmail.com>
2023-05-21 21:13:58 +0200

    feat: Changed curses renderer type signature

--- overengineered_snakes/renderers/curses.py
+++ overengineered_snakes/renderers/curses.py
@@ -1,6 +1,6 @@
 import curses
 
-from overengineered_snakes.handler import Handler
+from overengineered_snakes.backend.mapa import Mapa
 
 
 class CursesRenderer:
@@ -12,9 +12,9 @@ class CursesRenderer:
         for i in range(0, curses.COLORS):  # Curses shit
             curses.init_pair(i + 1, i, -1)
 
-    def render(self, handler: Handler) -> None:
-        for y_i, y in enumerate(range(0, len(handler.grid))):
-            y_array = handler.grid[y]
+    def render(self, mapa: Mapa) -> None:
+        for y_i, y in enumerate(range(0, len(mapa.grid))):
+            y_array = mapa.grid[y]
             for x_i, x in enumerate(y_array):
                 self.stdscr.addch(y_i, x_i, x.character, curses.color_pair(x.color))
         self.stdscr.refresh()
